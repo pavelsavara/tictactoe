b8648959a408f4fa497653f70d63ecac2d42060c
Pavel Savara <pavel.savara@ncr.com>
2019-01-05 14:51:07 +0100

    WIN

--- tttenv.py
+++ tttenv.py
@@ -3,7 +3,7 @@ import sys
 import gym
 import gym.spaces as spaces
 import gym.utils.seeding as seeding
-from game import Game, Board
+from game import Game, Board, TIE,X,O
 from agents import SmartAgent, RandomAgent
 
 class TicTacToeEnv(gym.Env):
@@ -22,30 +22,40 @@ class TicTacToeEnv(gym.Env):
         emptySqares = self.game.emptySquares()
         if move not in emptySqares:
             self.reset()
-            return self._get_observation(), -1.0, True, {reason: 'bad move, loose!'}
+            return self._get_observation(), -1.0, True, 
         self.game.move(move)
-        
-        winner = self.winner()
-        if winner == 'T':
+        winner = self.game.winner()
+
+        if winner == TIE:
             self.reset()
-            return self._get_observation(), 0.01, True, {reason: 'tie!'}
+            return self._get_observation(), 0.01, True, {'reason': 'tie1!'}
         
+        if winner == self.game.turn:
+            self.reset()
+            return self._get_observation(), -0.99, True, {'reason': 'you loose1!'}
+
         if winner:
             self.reset()
-            return self._get_observation(), 1.0, True, {reason: 'you win!'}
+            return self._get_observation(), 1.0, True, {'reason': 'you win1!'}
 
         oponentmove = self.oponent.getMove(self.game)
-        self.game.move(oponentmove)
 
-        if winner == 'T':
+        self.game.move(oponentmove)
+        winner = self.game.winner()
+
+        if winner == TIE:
             self.reset()
-            return self._get_observation(), 0.01, True, {reason: 'tie!'}
+            return self._get_observation(), 0.01, True, {'reason': 'tie2!'}
+
+        if winner == self.game.turn:
+            self.reset()
+            return self._get_observation(), 1.0, True, {'reason': 'you win2!'}
 
         if winner:
             self.reset()
-            return self._get_observation(), -0.99, True, {reason: 'you loose!'}
+            return self._get_observation(), -0.99, True, {'reason': 'you loose2!'}
 
-        return self._get_observation(), 0.0, False, {reason: 'not settled'}
+        return self._get_observation(), 0.0, False, {'reason': 'not settled'}
 
     def render(self, mode='human'):
         outfile = StringIO() if mode == 'ansi' else sys.stdout
